30262a4f6d325e2cc78d4bafe7ee3afacbb5977b
unknown <mh20011999@gmail.comgit config --global user.name mehedi132>
2020-03-21 08:13:43 +0600

    Create login and logout Activity

---
+++ app/src/main/java/com/example/lab03/SecondActivity.java
@@ -0,0 +1,41 @@
+package com.example.lab03;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity implements
+        View.OnClickListener{
+    Button backToMainActivityButton;
+    TextView nameTextView, studentIDTextView, userIDTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toast.makeText(SecondActivity.this,"Login Successful", Toast.LENGTH_SHORT).show();
+        backToMainActivityButton = (Button) findViewById(R.id.logoutButton);
+        backToMainActivityButton.setOnClickListener(this);
+        nameTextView = (TextView) findViewById(R.id.name);
+        studentIDTextView = (TextView) findViewById(R.id.StudentID);
+        userIDTextView = (TextView) findViewById(R.id.userIDShow);
+        Bundle bundle = getIntent().getExtras();
+        if(bundle!=null){
+            String name = bundle.getString("name");
+            String studentID = bundle.getString("studentId");
+            String userID = bundle.getString("user");
+            nameTextView.setText(name);
+            studentIDTextView.setText(studentID);
+            userIDTextView.setText(userID);
+        }
+    }
+    @Override
+    public void onClick(View v) {
+        Intent logoutIntent = new Intent(SecondActivity.this, MainActivity.class);
+        startActivity(logoutIntent);
+    }
+}
